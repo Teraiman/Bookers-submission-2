6145c43b396e653b3e3b941d50da8a032619644f
Teraiman <toshi.guitar.no1.9995@gmail.com>
2021-05-15 14:10:55 +0000

    8章:Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @books = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-resources:books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
   get 'top' => 'homes'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+
 end
