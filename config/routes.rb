007521e7ee70fe23f55acd05a596f475a2301eff
Teraiman <toshi.guitar.no1.9995@gmail.com>
2021-05-16 10:02:48 +0000

    8章:Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
   get 'top' => 'homes'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-
-end
+  resources :books
+  
+ end
